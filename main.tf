bbcf163d94286c8a67ddbc7fd28f05f55dc4baae
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-04 18:51:33 +0530

    providers_errors

--- main.tf
+++ main.tf
@@ -8,3 +8,18 @@ module "network_vpc" {
   private_cidr_block= each.value.private_cidr_block
 
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Or the version you need
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "us-east-1"  # Set your desired region
+}
