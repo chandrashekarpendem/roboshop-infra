e442069237f76b92af4b26febd5ec75bfa3feb18
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-04 18:34:26 +0530

    vpc_module

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,2 +1,10 @@
 env = "dev"
-cidr_block = "10.0.0.0/16"
+default_vpc_id ="vpc-017e4ed2b0978fa6f"
+
+vpc = {
+  main = {
+    cidr_block="10.0.0.0/16"
+    public_cidr_block=["10.0.0.0/24","10.0.1.0/24"]
+    private_cidr_block=["10.0.3.0/24","10.0.4.0/24"]
+  }
+}
--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 module "network_vpc" {
   source = "github.com/chandrashekarpendem/Terraform-module-vpc.git"
 
-  for_each    = var.network_vpc
+  for_each    = var.vpc
   cidr_block  = each.value.cidr_block
+  public_cidr_block= each.value.public_cidr_block
+  private_cidr_block= each.value.private_cidr_block
 
 }
