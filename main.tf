d33d80b2c66376e3169941b203af98c428486730
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-04 18:45:05 +0530

    shared_library

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "network_vpc" {
   source = "github.com/chandrashekarpendem/Terraform-module-vpc.git"
 
   for_each    = var.vpc
+  env         = var.env
   cidr_block  = each.value.cidr_block
   public_cidr_block= each.value.public_cidr_block
   private_cidr_block= each.value.private_cidr_block
