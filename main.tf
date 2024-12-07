f992463208951df23bdae845e8e4ad8e5a9143bd
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-07 19:23:46 +0530

    network_adjustment

--- main.tf
+++ main.tf
@@ -17,8 +17,6 @@ module "subnets" {
   for_each   = var.subnets
   cidr_block = each.value.cidr_block
   name       = each.value.name
-  vpc_name = each.value.vpc_name
-  gateway_id = module.network_vpc.gateway_id
   availability_zone = each.value.availability_zone
   vpc_id = lookup(lookup(module.network_vpc,each.value.vpc_name,null), "vpc_id", null)
   vpc_peering_connection_id = lookup(lookup(module.network_vpc,each.value.vpc_name,null), "vpc_peering_connection_id", null)
