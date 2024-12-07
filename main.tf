039be840701beeaf30025c747803a18cc8282c1b
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-07 19:00:19 +0530

    network_adjustment

--- main.tf
+++ main.tf
@@ -8,23 +8,23 @@ module "network_vpc" {
 
 }
 
-module "subnets" {
-  source              = "./tf-module-subnets"
-  env                 = var.env
-  default_vpc_id      = var.default_vpc_id
-
-
-  for_each   = var.subnets
-  cidr_block = each.value.cidr_block
-  name       = each.value.name
-  vpc_name = each.value.vpc_name
-  gateway_id = module.network_vpc
-  availability_zone = each.value.availability_zone
-  vpc_id = lookup(lookup(module.network_vpc,each.value.vpc_name,null), "vpc_id", null)
-  vpc_peering_connection_id = lookup(lookup(module.network_vpc,each.value.vpc_name,null), "vpc_peering_connection_id", null)
-
-
-}
+#module "subnets" {
+#  source              = "./tf-module-subnets"
+#  env                 = var.env
+#  default_vpc_id      = var.default_vpc_id
+#
+#
+#  for_each   = var.subnets
+#  cidr_block = each.value.cidr_block
+#  name       = each.value.name
+#  vpc_name = each.value.vpc_name
+#  gateway_id = module.network_vpc
+#  availability_zone = each.value.availability_zone
+#  vpc_id = lookup(lookup(module.network_vpc,each.value.vpc_name,null), "vpc_id", null)
+#  vpc_peering_connection_id = lookup(lookup(module.network_vpc,each.value.vpc_name,null), "vpc_peering_connection_id", null)
+#
+#
+#}
 
 output "vpc" {
   value = module.network_vpc
