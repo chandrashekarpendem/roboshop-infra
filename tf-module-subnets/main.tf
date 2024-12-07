72f2e3aa2f55859002acc292168a2a1e0390c98f
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-07 20:00:46 +0530

    network_adjustment2

--- tf-module-subnets/main.tf
+++ tf-module-subnets/main.tf
@@ -9,11 +9,6 @@ resource "aws_subnet" "subnet" {
 resource "aws_route_table" "route_table" {
   vpc_id = var.vpc_id
 
-  route {
-    count = var.igw ?  1 : 0
-    cidr_block = "0.0.0.0/0"
-    gateway_id = var.gateway_id
-  }
 
   route {
     cidr_block                = data.aws_vpc.default_vpc_info.cidr_block
@@ -28,4 +23,10 @@ resource "aws_route_table_association" "route_table_association_to_subnet" {
   route_table_id = aws_route_table.route_table.id
   subnet_id = aws_subnet.subnet.*.id[count.index]
 
+}
+
+resource "aws_route" "igw_route" {
+  count = var.igw ? 1 : 0
+  route_table_id = aws_route_table.route_table.id
+  gateway_id = var.gateway_id
 }
