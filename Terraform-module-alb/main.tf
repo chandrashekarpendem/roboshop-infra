700432e646be1d620529f675154662aab8e5c310
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-19 16:41:27 +0530

    listener_and-rules

--- Terraform-module-alb/main.tf
+++ Terraform-module-alb/main.tf
@@ -12,7 +12,14 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks      = var.allow_cidr_alb
 
   }
+  ingress {
+    description      = "HTTP-ALB"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_cidr_alb
 
+  }
   egress {
     from_port        = 0
     to_port          = 0
