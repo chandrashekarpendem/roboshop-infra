00fd4396879ce3950bdef10739f94345c892565c
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-19 16:07:47 +0530

    listener_and-rules

--- Terraform-module-app/data.tf
+++ Terraform-module-app/data.tf
@@ -1,5 +1,6 @@
 data "aws_ami" "ami_id_robo" {
   most_recent = true
+  name_regex = "Chandra"
   owners = [225989332181]
 }
 data "aws_kms_key" "roboshop_key" {
--- Terraform-module-rabbittmq/data.tf
+++ Terraform-module-rabbittmq/data.tf
@@ -1,5 +1,6 @@
 data "aws_ami" "centos8" {
   most_recent = true
+  name_regex = "Chandra"
   owners = [225989332181]
 }
 data "aws_kms_key" "roboshop_key" {
