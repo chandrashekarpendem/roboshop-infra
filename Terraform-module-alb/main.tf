49cba9359224a8e1162651b0a0fcbabb54f691c4
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-20 12:55:08 +0530

    load_balancers

--- Terraform-module-alb/main.tf
+++ Terraform-module-alb/main.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "alb_sg" {
 
 
   ingress {
-    description      = "HTTP-ALB"
+    description      = "HTTP"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
