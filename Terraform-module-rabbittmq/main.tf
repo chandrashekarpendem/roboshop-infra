8556448452bc598e628211b341d98473e6e9f3f4
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-20 19:01:42 +0530

    rabbitmq

--- Terraform-module-rabbittmq/main.tf
+++ Terraform-module-rabbittmq/main.tf
@@ -105,36 +105,6 @@ resource "aws_security_group" "rabbitmq" {
 }
 
 
-// as our application code for rabbitmq  doesn't designed to deal with secured protocol of rabbitmq we are going with ec2 instance rather than the mq_broker
-
-#resource "aws_mq_broker" "rabbitmq" {
-#  broker_name        = "${var.env}-rabbittmq_mq_broker"
-#  deployment_mode    = var.deployment_mode
-#  engine_type        = var.engine_type
-#  engine_version     = var.engine_version
-#  host_instance_type = var.host_instance_type
-#  security_groups    = [aws_security_group.rabbitmq.id]
-#  subnet_ids = var.deployment_mode == "SINGLE_INSTANCE" ? [var.subnet_ids[0]] : var.subnet_ids
-#
-##  configuration {
-##    id       = aws_mq_configuration.rabbitmq.id
-##    revision = aws_mq_configuration.rabbitmq.latest_revision
-##  }
-#
-#  encryption_options {
-#    use_aws_owned_key = false
-#    kms_key_id = data.aws_kms_key.key.arn
-#  }
-#
-#  user {
-#    username = data.aws_ssm_parameter.rabbitmq_ADMIN_USER.value
-#    password = data.aws_ssm_parameter.rabbitmq_ADMIN_USER.value
-#  }
-#}
-
-
-
-
 resource "aws_spot_instance_request" "rabbitmq_instance" {
   ami = data.aws_ami.centos8.image_id
   instance_type = "t3.small"
@@ -169,8 +139,3 @@ resource "aws_route53_record" "rabbitmq_DNS_record" {
 }
 
 
-#resource "aws_ssm_parameter" "rabbitmq_endpoint" {
-#  name  = "${var.env}.rabbitmq.endpoint"
-#  type  = "String"
-#  value = replace(replace(aws_mq_broker.rabbitmq.instances.0.endpoints.0,"amqps://", ""), ":5671", "")
-#}
